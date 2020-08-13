e5bd9a216d6c857d9f27c653c974fec7e3eca736
Nick Butler <nwmbutler@gmail.com>
2020-08-13 16:44:34 +0100

    get google/robots.txt

--- hello.go
+++ hello.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
-)
-
-const (
-	a = iota
-	b
-	c
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-
-	fmt.Printf("%v\n", a)
-	fmt.Printf("%v\n", b)
-	fmt.Printf("%v\n", c)
+	res, err := http.Get("http://www.google.com/robots.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	robots, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", robots)
 }
